7ecfa7018a607b5d49746cce16dc4892cba9d9c7
VictorBitancourt <victorsodre209@gmail.com>
2026-03-19 19:44:36 -0300

    Redireciona para registro do admin quando BD está vazia no login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   allow_unauthenticated_access only: %i[ new create ]
+  before_action :redirect_to_setup, only: %i[ new create ]
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_path, alert: I18n.t("controllers.sessions.rate_limited") }
 
   def new
