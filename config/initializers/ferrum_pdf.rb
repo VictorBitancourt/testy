6a76c03fdac263c06ce73870aafda8643ba91879
VictorBitancourt <victorsodre209@gmail.com>
2026-04-01 12:52:18 -0300

    correções no design do report

--- config/initializers/ferrum_pdf.rb
+++ config/initializers/ferrum_pdf.rb
@@ -3,4 +3,9 @@
 FerrumPdf.configure do |config|
   config.process_timeout = 30
   config.pdf_options.print_background = true
+  config.browser_options = {
+    "no-sandbox": true,
+    "disable-dev-shm-usage": true,
+    "disable-gpu": true
+  }
 end
